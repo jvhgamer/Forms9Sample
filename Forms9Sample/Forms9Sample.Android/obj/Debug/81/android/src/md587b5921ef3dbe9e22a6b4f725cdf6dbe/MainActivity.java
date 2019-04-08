9831570e590f74ff0d9b614e065961d3c1504f1f
Jack Higgins <Jack.Higgins@corp.callawaygolf.com>
2019-04-08 10:39:45 -0700

    first commit

---
+++ Forms9Sample/Forms9Sample.Android/obj/Debug/81/android/src/md587b5921ef3dbe9e22a6b4f725cdf6dbe/MainActivity.java
@@ -0,0 +1,47 @@
+package md587b5921ef3dbe9e22a6b4f725cdf6dbe;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Forms9Sample.Droid.MainActivity, Forms9Sample.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Forms9Sample.Droid.MainActivity, Forms9Sample.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
